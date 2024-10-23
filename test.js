cee9faf4df56107e640ad8b22e48f7c0705a76f7
Navneet Kaur <navkaurneet1@gmail.com>
2024-10-22 22:45:11 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,16 @@
 const request = require('supertest');
-const app = require('./index.js'); // assuming your app is exported from index.js
+const app = require('./index');
+let server;
+
+beforeAll(() => {
+  // Start the server before running tests
+  server = app.listen(4000); // Start on a test-specific port
+});
+
+afterAll((done) => {
+  // Close the server after tests are done
+  server.close(done);
+});
 
 test('GET / should return 200', async () => {
   const res = await request(app).get('/');
