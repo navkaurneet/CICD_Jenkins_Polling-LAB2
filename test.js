55a8a4e44bd0944d16075e26fe3a94705afecbc0
Navneet Kaur <navkaurneet1@gmail.com>
2024-10-22 22:56:35 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../index'); // Import your Express app
+const app = require('index'); // Import your Express app
 
 describe('GET /', () => {
   it('should return a 200 status and Hello, World! message', async () => {
