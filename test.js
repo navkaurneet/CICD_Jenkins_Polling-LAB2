ce3df5e31bb0591072e2b828c903c692817bf7e1
navkaurneet <navkaurneet1@gmail.com>
2024-10-22 22:32:03 -0400

    adding files

---
+++ test.js
@@ -0,0 +1,8 @@
+const request = require('supertest');
+const app = require('./index'); // assuming your app is exported from index.js
+
+test('GET / should return 200', async () => {
+  const res = await request(app).get('/');
+  expect(res.statusCode).toEqual(200);
+  expect(res.text).toContain('Hello, Continuous Integration with GitHub Actions!');
+});
